81a47963d04b72847358fe6ae1f738b7faefccd3
adamman722 <tarpeyadam@gmail.com>
2022-03-09 20:45:58 -0500

    Building a new app

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Calculator() {
+  return (
+    <div className="idk-container">
+      <div className="flex-container">Calculator</div>
+      <div className="numbers-box">nuggets</div>
+    </div>
+  );
+}
+
+export default Calculator;
